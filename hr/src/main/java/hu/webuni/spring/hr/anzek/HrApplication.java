358922a946ee8666d8cb5241b087cdd31e7439a5
User <User@DESKTOP-LENOVOT460S.home>
2021-07-06 16:12:51 +0200

    hr projekt 1 heti gyakorlata eclepsen keresztül sikeres GitHub letrehozas utáni módosítás Gábor ismertte, hogy milyen hierarchiában próbál injektálni... végelgesített 1 heti állapot 4 dolgozóra és már a thymeleaf-al függőséggel is bővítve

--- hr/src/main/java/hu/webuni/spring/hr/anzek/HrApplication.java
+++ hr/src/main/java/hu/webuni/spring/hr/anzek/HrApplication.java
@@ -36,6 +36,7 @@ public class HrApplication implements CommandLineRunner {
         //System.out.println("1. dolgozo neve______________ : " + adiap.getEmployee().getWorkerName() );
         //System.out.println("1. dolgozo fizuja___________ : " + adiap.getEmployee().getMonthlySalary() );
        
+        // 1
         employee.setId( 1L );
         employee.setJobPosition( "Fo_fo_Mufti" );
         employee.setMonthlySalary( 500000 );
@@ -59,6 +60,84 @@ public class HrApplication implements CommandLineRunner {
         System.out.println( "_Fizetes valtozas_____________: " );
         System.out.println( "   a letoltott munkaido utani % " + employee.getTorzsGarda() );
         System.out.println( "   dolgozo old_fizu__________ : " + oldFizu );
-        System.out.println( "1. dolgozo uj_fizu___________ : " + employee.getMonthlySalary() );
+        System.out.println( "   dolgozo uj_fizu___________ : " + employee.getMonthlySalary() );
+        
+        // 2 
+        employee.setId( 2L );
+        employee.setJobPosition( "Fo_al_Mufti" );
+        employee.setMonthlySalary( 400000 );
+        employee.setStartOfEmployment( LocalDateTime.of( 2010, 11, 20, 0, 0, 0 ) );
+        employee.setWorkerName( "Albínó Zénó" );        
+        
+        // elmentjuk a fizeteset:
+        oldFizu = employee.getMonthlySalary();        
+        
+        // megvaltoztatjuk a fizeteset:
+        salaryService.setEmployee( employee );        
+  
+        // modositjuk, majd vissza is toltjuk a mododosult adatokat:
+        employee = salaryService.incomeService();        
+        
+        // kiiratjuk a Munkavallaloi aktualis adatokat:
+        System.out.println( "2. dolgozokod________________ : " + employee.getId() );
+        System.out.println( "   dolgozo neve______________ : " + employee.getWorkerName() );
+        System.out.println( "   dolgozo munkakore_________ : " + employee.getJobPosition() );
+        System.out.println( "   dolgozo munaviszony kezdet : " + employee.getStartOfEmployment() );
+        System.out.println( "_Fizetes valtozas_____________: " );
+        System.out.println( "   a letoltott munkaido utani % " + employee.getTorzsGarda() );
+        System.out.println( "   dolgozo old_fizu__________ : " + oldFizu );
+        System.out.println( "   dolgozo uj_fizu___________ : " + employee.getMonthlySalary() );
+
+        // 3         
+        employee.setId( 3L );
+        employee.setJobPosition( "Al_fo_Mufti" );
+        employee.setMonthlySalary( 300000 );
+        employee.setStartOfEmployment( LocalDateTime.of( 2017, 7, 11, 0, 0, 0 ) );
+        employee.setWorkerName( "Lavór Tátika" );        
+        
+        // elmentjuk a fizeteset:
+        oldFizu = employee.getMonthlySalary();        
+        
+        // megvaltoztatjuk a fizeteset:
+        salaryService.setEmployee( employee );        
+  
+        // modositjuk, majd vissza is toltjuk a mododosult adatokat:
+        employee = salaryService.incomeService();        
+        
+        // kiiratjuk a Munkavallaloi aktualis adatokat:
+        System.out.println( "3. dolgozokod________________ : " + employee.getId() );
+        System.out.println( "   dolgozo neve______________ : " + employee.getWorkerName() );
+        System.out.println( "   dolgozo munkakore_________ : " + employee.getJobPosition() );
+        System.out.println( "   dolgozo munaviszony kezdet : " + employee.getStartOfEmployment() );
+        System.out.println( "_Fizetes valtozas_____________: " );
+        System.out.println( "   a letoltott munkaido utani % " + employee.getTorzsGarda() );
+        System.out.println( "   dolgozo old_fizu__________ : " + oldFizu );
+        System.out.println( "   dolgozo uj_fizu___________ : " + employee.getMonthlySalary() );
+
+        // 4
+        employee.setId( 4L );
+        employee.setJobPosition( "Al_al_Mufti" );
+        employee.setMonthlySalary( 200000 );
+        employee.setStartOfEmployment( LocalDateTime.of( 2005, 8, 4, 0, 0, 0 ) );
+        employee.setWorkerName( "Sivár Albínó" );        
+        
+        // elmentjuk a fizeteset:
+        oldFizu = employee.getMonthlySalary();        
+        
+        // megvaltoztatjuk a fizeteset:
+        salaryService.setEmployee( employee );        
+  
+        // modositjuk, majd vissza is toltjuk a mododosult adatokat:
+        employee = salaryService.incomeService();        
+        
+        // kiiratjuk a Munkavallaloi aktualis adatokat:
+        System.out.println( "4. dolgozokod________________ : " + employee.getId() );
+        System.out.println( "   dolgozo neve______________ : " + employee.getWorkerName() );
+        System.out.println( "   dolgozo munkakore_________ : " + employee.getJobPosition() );
+        System.out.println( "   dolgozo munaviszony kezdet : " + employee.getStartOfEmployment() );
+        System.out.println( "_Fizetes valtozas_____________: " );
+        System.out.println( "   a letoltott munkaido utani % " + employee.getTorzsGarda() );
+        System.out.println( "   dolgozo old_fizu__________ : " + oldFizu );
+        System.out.println( "   dolgozo uj_fizu___________ : " + employee.getMonthlySalary() );        
     }    
 }
