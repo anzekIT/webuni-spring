fbb33d3b07fad21926a320bb1b17d441c9b9ee44
User <User@DESKTOP-LENOVOT460S.home>
2021-07-08 10:30:42 +0200

    hr projekt 1 heti gyakorlata eclepsen keresztül sikeres GitHub letrehozas utáni módosítás Gábor ismertte, hogy milyen hierarchiában próbál injektálni... végelgesített 1 heti állapot 4 dolgozóra és már a thymeleaf-al függőséggel is bővítve A 2. házifeladat elsőrésze (a html-táblázatig)

--- hr/src/main/java/hu/webuni/spring/hr/anzek/webcontrol/EmployeeWebController.java
+++ hr/src/main/java/hu/webuni/spring/hr/anzek/webcontrol/EmployeeWebController.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  *
@@ -24,11 +25,14 @@ public final class EmployeeWebController {
     int weboldalFrissitesekSzama = 0;
     
     private final List<EmployeeDto> allEmployees = new ArrayList<>();
-    // nem konstruktor, hanem egy inicializalo blokk:    
+    
+    // INIT BLOKK:
+    // nem konstruktor, hanem egy inicializalo blokk:  
+    void InitController()
     {
        
         allEmployees.add( new EmployeeDto ( 1L, "Kovács Pistika", "Fo_fo_Mufti", 500000, LocalDateTime.of( 2015, 1, 1, 0, 0, 0 ) ) );
-        allEmployees.add( new EmployeeDto ( 1L, "Siker Kulcsa", "Fo_al_Mufti", 400000, LocalDateTime.of( 2010, 1, 1, 0, 0, 0 ) ) );
+        allEmployees.add( new EmployeeDto ( 2L, "Siker Kulcsa", "Fo_al_Mufti", 400000, LocalDateTime.of( 2010, 1, 1, 0, 0, 0 ) ) );
     }
     
     // ez azt jelenti, hogy a gyokerre erkezo kereseket 
@@ -43,13 +47,30 @@ public final class EmployeeWebController {
     /**
      * Az "./employees" URL-re erkezo keres<br>
      * a "model"- adattartalomba behelyez egy "AllEmployees" adatot (egyelore csak memoriabol)<br>
-     * @param model egy MAp kollekcio<br>
+     * @param model egy MAP kollekcio<br>
      * @return viszaadja z employees.html" tartalmat a beilleszett valtozo adatokkal<br>
      */
     @GetMapping("/employees")
-    public String listEmployees( Map<String, Object> model ){
+    public String getListEmployees( Map<String, Object> model ){
     
         model.put( "outemployees", allEmployees );
+        
+        // Ez pedig itt kell, hogy mukodjon a (ha lesz) a Html-oldalon a bekeres:
+        model.put( "newEmployee", new EmployeeDto() );
+        
         return "employees.html";
     }
+    
+    /**
+     * Az "./employees" URL-re kuldendo "POST keres" elokeszitese<br>
+     * a "model"- adattartalomba behelyez egy "AllEmployees" adatot (egyelore csak memoriabol)<br>
+     * @param employeeDto egy az Employees kollekcio eleme<br>
+     * @return viszaadja z employees.html" tartalmat a beilleszett valtozo adatokkal<br>
+     */
+    @PostMapping("/employees")
+    public String addtListEmployees( EmployeeDto employeeDto ){
+    
+        allEmployees.add(employeeDto) ;
+        return "employees.html";
+    }    
 }
