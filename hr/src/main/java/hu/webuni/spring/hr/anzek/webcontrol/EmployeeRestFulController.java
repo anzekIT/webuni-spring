55a7b5d2c0db153ca92c32a970f870950518728c
User <User@DESKTOP-LENOVOT460S.home>
2021-07-13 11:14:07 +0200

    hr projekt 1 heti gyakorlata eclepsen keresztül sikeres GitHub letrehozas utáni módosítás Gábor ismertte, hogy milyen hierarchiában próbál injektálni... végelgesített 1 heti állapot 4 dolgozóra és már a thymeleaf-al függőséggel is bővítve A 2. házifeladat elsőrésze: az adatbekérésig és a html-megjelenítésig REST-api az első lépésekben már kiiratom az adatokat KIEGÉSZÍTVE (próba) a privátFelhő.html-el,és a tervezett tartalommal Teljesen kész a postmennel is tesztelve (bár ott voltak furaságok)

--- hr/src/main/java/hu/webuni/spring/hr/anzek/webcontrol/EmployeeRestFulController.java
+++ hr/src/main/java/hu/webuni/spring/hr/anzek/webcontrol/EmployeeRestFulController.java
@@ -11,7 +11,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,36 +28,140 @@ import org.springframework.web.bind.annotation.RestController;
  * A @RequestMapping annotacio pedig ahhoz kell, hogy minden handler-metodus, amely majd egyéb URI -ket definial...<br> 
  * ...(ebben a kontrollerben) az relative a @RequestMapping(mappa) -hoz ertendo<br>
  * FONTOS!<br>
- * A REST-kontrollerek NEM DOLGOZNAK A MODEL-el!<br>
- * Itt mindig ADATOKAT adunk vissza!<br>
+ * A REST-kontrollerek NEM A MODEL-el DOLGOZNAK (Azt a webControllerek hasznaljak, abban helyezik el a valaszt)!<br>
+ * Itt kozvetlenul ADATOKAT adunk vissza!<br>
  * @author User
  */
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeRestFulController {
 
-    // lesz olyan funkcio, hogy adott ID-ju tetelt adjunk vissza, hogy modositsuk, hogy toroljuk, stb.   
-    // Itt nem LISTA-ban taroljuk (arrayList-ben), mert az csak linearis szekvenciaval jarhato be
-    // Ezert most a MAP<K,V> -ben taroljuk le az Employee peldanyokat es az ID-juket hasznaljuk KULCS-kent :
-    
+    /**
+     * lesz olyan funkcio, hogy adott ID-ju tetelt adjunk vissza, hogy modositsuk, hogy toroljuk, stb.   
+     * Itt nem LISTA-ban taroljuk (arrayList-ben), mert az csak linearis szekvenciaval jarhato be
+     * Ezert most a MAP<K,V> -ben taroljuk le az Employee peldanyokat es az ID-juket hasznaljuk KULCS-kent : 
+     */
     private final Map< Long, EmployeeDto > employees = new HashMap<>();
     
-    // INIT BLOKK:
-    //
-    // Az adatokat most meg csak egy MAP<K,V>-ben taroljuk (nem adatbazisban)
-    //
-    // nem konstruktor, nem metodus hanem egy szimpla inicializalo blokk:    
+    /**
+     * INIT BLOKK:<br>
+     * Az adatokat most meg csak egy MAP<K,V>-ben taroljuk (nem adatbazisban)<br>
+     * nem konstruktor, nem metodus hanem egy szimpla inicializalo blokk: <br>      
+     */   
     {
        
         this.employees.put( 1L, new EmployeeDto ( 1L, "Kovács Pistika", "Fo_fo_Mufti", 500000, LocalDateTime.of( 2015, 1, 1, 0, 0, 0 ) ) );
         this.employees.put( 2L, new EmployeeDto ( 2L, "Siker Kulcsa", "Fo_al_Mufti", 400000, LocalDateTime.of( 2010, 1, 1, 0, 0, 0 ) ) );
-    }
-    
+    }  
+
+    /**
+     * GET-METHOD<br>
+     * Az elso vegpont<br> 
+     * Egy vegpont = valamelyURI<br>
+     * Elozor is lesz egy GetMapping amely a "gyokerbol" dolgozik,<br> 
+     * es amely viszaad a keresre majd egy listat<br>
+     * @return egy lista-kollekciot ad vissza<br>
+     */
     @GetMapping
     public List<EmployeeDto> getAll(){
     
+        // minden objektum value() erteke egy kollekcio-lista:
+        // igy pedig lenyegeben egy tomblista lesz belole:
+        // Alapvetoen egy JSON objectum jon letre:
         return new ArrayList<>( this.employees.values() );
     }
     
+
+    /**
+     * GET-METHOD<br>
+     * Kerjunk le EGY KONKRET "id"-vel rendelkezo elemet:<br>
+     * Ez szinten egy GET -kerest valosit meg,<br>
+     * DE !<br>
+     * Itt van egy fontos "tolaleka" a mappa (utvonal) felmappingelesnel, megpedig az ADATMEZO neve a keres LEGVEGEN egy PER jellel elvalasztva:<br>
+     * igy kell megni:<br>
+     * @param id @PathVariable annotacioval kell beolvasni, majd a parameterben "beadni" a GET keresben atadott erteket:<br>
+     * @return visszaad egy body - entitast<br>
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<EmployeeDto> getById( @PathVariable long id ){
+        
+        // a MAP kollekciobol kiolvassa az "id" -ben levő kulcserteknek megfelelo entitast
+        final EmployeeDto employeeDto = employees.get( id );
+        // a visszatero torzs (body)
+        ResponseEntity entity;
+        // erdekesseg:
+        // Ha LETEZO kodot kerdeznk le : "200 OK" - lesz a szerver valasza
+        // Ha NEM LETEZO -t kerdezunk le, akkor is "200 OK" -t ad vissza (ures erteket)
+        // .... de a konvencio szerint ezt a "404 Page Not Found" uzenetre szoktak cserelni.
+        if ( employeeDto != null ){
+            
+            // ez le is gartja a "body"-t
+            entity = ResponseEntity.ok( employeeDto );
+        }else{
+            
+            // a noFound(9 nem gyartja le, tehat le kell gyartatni a build() metodusaval:
+            entity = ResponseEntity.notFound().build();
+        }
+        return entity;
+    }
+ 
+    /**
+     * POST-METHOD<br>
+     * Letrehozas<br>
+     * @param employeeDto az uj berogzitendo peldany<br>
+     * @return viszaadja a parameterben szereplo uj peldanyt<br> 
+     */
+    @PostMapping
+    public EmployeeDto createEmployeeDto( @RequestBody EmployeeDto employeeDto ){
+        
+        employees.put( employeeDto.getId(), employeeDto );
+        return employeeDto;
+    }
     
+    /**
+     * PUT-METHOD<br>
+     * Egy uj tetel behelyezese az adhalmazba<br>
+     * @param id az ujonnan rogizendo tetel ID-je<br>
+     * @param employeeDto az ujonnan rogzitendo entitas<br>
+     * @return visszaadja: sikeres volt -e a torles (lezűtezett-e elotte) vagy sem<br> 
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<EmployeeDto> modifyEmployeeDto( @PathVariable long id, @RequestBody EmployeeDto employeeDto ){
+    
+        ResponseEntity entity;
+        employeeDto.setId(id);
+        
+        if ( ! employees.containsKey(id) ){
+            
+            entity = ResponseEntity.notFound().build();
+        }else{
+            
+            employees.put(id, employeeDto);
+            entity = ResponseEntity.ok( employeeDto );
+        }
+                
+        return entity;
+    }
+    
+    /**
+     * DELETE METHOD<br>
+     * @param id a torlesre jelolt tetel ID azonositoja<br>
+     * @return visszaadja: sikeres volt -e a torles (lezűtezett-e elotte) vagy sem<br>
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteEmployeeDto( @PathVariable long id ){
+    
+        ResponseEntity entity;
+ 
+        if ( ! employees.containsKey(id) ){
+            
+            entity = ResponseEntity.notFound().build();
+        }else{
+            
+            employees.remove(id);
+            entity = ResponseEntity.ok( "Torles rendben!" );
+        }
+                
+        return entity;        
+    }
 }
